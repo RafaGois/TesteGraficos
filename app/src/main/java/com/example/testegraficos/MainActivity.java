86391ede95773a4c41020e520557c82912f1f271
Rafael Fontana Becker Gois <joefontanabg@gmail.com>
2022-09-27 17:36:32 -0300

    att

--- app/src/main/java/com/example/testegraficos/MainActivity.java
+++ app/src/main/java/com/example/testegraficos/MainActivity.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
@@ -27,5 +30,27 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayList<BarEntry> barEntries = new ArrayList<>();
         ArrayList<PieEntry> pieEntries = new ArrayList<>();
+
+        for (int i = 0; i < 10;i++) {
+            float value = (float) (i * 10.0);
+
+            BarEntry barEntry = new BarEntry(i,value);
+
+            PieEntry pieEntry = new PieEntry(i,value);
+
+            barEntries.add(barEntry);
+            pieEntries.add(pieEntry);
+        }
+
+        BarDataSet barDataSet = new BarDataSet(barEntries,"Graf 1");
+
+        barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        barDataSet.setDrawValues(false);
+
+        barChart.setData(new BarData(barDataSet));
+
+        barChart.animateY(5000);
+
+
     }
 }
